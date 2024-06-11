b96c822998560b1a49c857df47dfd836cb171256
Svale09 <svalinaivan09+git@gmail.com>
2024-06-11 12:08:17 +0200

    Positioned the Reserve a desk button to the bottom of the screen

--- app/src/main/java/com/example/deskly/ui/ReserveDesk/ReserveDeskScreen.kt
+++ app/src/main/java/com/example/deskly/ui/ReserveDesk/ReserveDeskScreen.kt
@@ -2,13 +2,15 @@ package com.example.deskly.ui.ReserveDesk
 
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,28 +41,47 @@ fun ReserveDeskScreen(
             )
         }
     ) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
-            Row(
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+        ) {
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.CenterHorizontally)
-                    .padding(top = 10.dp),
-                horizontalArrangement = Arrangement.SpaceAround
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                OfficePicker(offices = mockOffices)
-                DatePicker()
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        OfficePicker(offices = mockOffices)
+                        DatePicker()
+                    }
+                    //TODO create a function for displaying the desk grid
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                CustomButton(
+                    text = "Reserve a desk",
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 50.dp)
+                        .padding(bottom = 30.dp)
+                )
             }
-            //TODO create a function for displaying the desk grid
-            CustomButton(
-                text = "Reserve a desk",
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .padding(horizontal = 50.dp)
-                    .padding(vertical = 10.dp)
-            )
         }
     }
 }
+
+@Composable
+fun DeskGrid(){
+    //TODO
+}
+
 
 @Preview
 @Composable
