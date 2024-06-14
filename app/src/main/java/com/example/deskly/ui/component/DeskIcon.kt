714e40ac8deada895fb20948f6d142f30b3a891a
Svale09 <svalinaivan09+git@gmail.com>
2024-06-14 22:33:12 +0200

    Finished the desk icon

--- app/src/main/java/com/example/deskly/ui/component/DeskIcon.kt
+++ app/src/main/java/com/example/deskly/ui/component/DeskIcon.kt
@@ -1,48 +1,60 @@
+package com.example.deskly.ui.component
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.deskly.R
+import com.example.deskly.ui.theme.primaryBlue
+import com.example.deskly.ui.theme.primaryRed
 
 @Composable
 fun DeskIcon(
     isReserved: Boolean
 ) {
-    var backgroundColor by remember { mutableStateOf(if (isReserved) Color.Red else Color.Transparent) }
+    var backgroundColor by remember { mutableStateOf(if (isReserved) primaryRed else Color.Transparent) }
 
     Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier
             .height(48.dp)
             .width(48.dp)
+            .clip(RoundedCornerShape(8.dp))
             .background(backgroundColor)
             .let {
                 if (isReserved) {
                     it
                 } else {
                     it.clickable {
-                        backgroundColor = Color.Blue
+                        backgroundColor =
+                            if (backgroundColor == primaryBlue) Color.Transparent else primaryBlue
                     }
                 }
             }
     ) {
         Icon(
-            imageVector = Icons.Default.Favorite,
+            imageVector = ImageVector.vectorResource(id = R.drawable.desk),
             contentDescription = null,
             //tint = if (isReserved) Color.Gray else MaterialTheme.colorScheme.primary, // Change icon color if needed
             modifier = Modifier
-                .width(24.dp)
-                .height(24.dp)
+                .width(30.dp)
+                .height(30.dp)
         )
     }
 }
@@ -50,5 +62,5 @@ fun DeskIcon(
 @Preview
 @Composable
 fun DeskIconPreview() {
-    DeskIcon(isReserved = true)
+    DeskIcon(isReserved = false)
 }
