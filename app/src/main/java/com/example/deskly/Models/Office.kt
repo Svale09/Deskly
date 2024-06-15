86be18c28245685c1b874d862792870cabae6bda
Svale09 <svalinaivan09+git@gmail.com>
2024-06-15 15:07:23 +0200

    Changed the Office model, and the mock offices var

--- app/src/main/java/com/example/deskly/Models/Office.kt
+++ app/src/main/java/com/example/deskly/Models/Office.kt
@@ -1,16 +1,15 @@
 package com.example.deskly.Models
 
 data class Office(
-    val id: String,
     val name: String,
-    val location: String
+    val desks: List<Desk>
 )
 
 var mockOffices: List<Office> = listOf(
-    Office("OS", "Osijek", "Osijek"),
-    Office(id = "SP", name = "Split", location = "Split"),
-    Office(id = "ZG", name = "Zagreb", location = "Beograd"),
-    Office(id = "RI", name = "Rijeka", location = "Novi Sad"),
+    Office("Osijek", mockDesks),
+    Office("Split", mockDesks),
+    Office("Zagreb", mockDesks),
+    Office("Rijeka", mockDesks),
 )
 
 
